a65b873d0a2006244e7256d977b6add5c20834f4
RubsRafa <rubiarafaela2000@gmail.com>
2024-02-26 16:18:33 -0300

    chore: set colors, spaces and typography styles

---
+++ src/styles/Typography.tsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Title_Big = styled.h1`
+  font-size: var(--fs-24);
+  font-weight: var(--fw-500);
+  line-height: var(--lh-2);
+`
+
+export const Title_Medium = styled.h2`
+  font-size: var(--fs-16);
+  font-weight: var(--fw-500);
+  line-height: var(--lh-1);
+`
+
+export const Title_Refular = styled.h3`
+  font-size: var(--fs-16);
+  font-weight: var(--fw-400);
+  line-height: var(--lh-1);
+`
